5df1aecbb146d4b52755356614a19c5e64f623c9
Kunal <kunaltailor5555@gmail.com>
2025-06-11 11:47:41 +0530

    frontend

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 
-export const BASE_URL =  "https://proconnect-linkedin-clone.onrender.com";
+export const BASE_URL =  "https://kunal-connect.onrender.com";
 
   export const clientServer = axios.create({
   baseURL: BASE_URL,
